733d08e54b5c6a8c14fab0c8f305536665842550
vinay putta <vinayputta@vinays-MacBook-Air.local>
2024-08-30 10:03:03 +0530

    simplified gin

--- main.go
+++ main.go
@@ -1,18 +1,96 @@
 package main
 
 import (
-	"github.com/go-2024/internal"
-	"github.com/go-2024/routes"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-2024/database"
+	"github.com/go-2024/models"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func main() {
-	server, err := internal.NewServer()
+	// server, err := internal.NewServer()
+	// if err != nil {
+	// 	panic("")
+	// }
+
+	// routes.PostRoutes(server)
+	var err error
+	DB, err = database.Init()
 	if err != nil {
-		panic("")
+		log.Fatal("eroor")
 	}
+	router := gin.Default()
 
-	routes.PostRoutes(server)
+	router.GET("/getposts", GetPosts)
+	router.POST("/createpost", CreatePost)
+	router.GET("/getpost/:id", GetPostByID)
+	router.DELETE("/deletepost/:id", DeletePostByID)
+	router.PUT("/updatepost/:id", UpdatePostByID)
 
-	server.S.Run(":8080")
+	router.Run(":8080")
+
+}
+
+func GetPosts(c *gin.Context) {
+	var posts []models.Post
+	if result := DB.Find(&posts); result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"err": result.Error.Error(),
+		})
+		return
+	}
+	c.JSON(200, posts)
+}
+
+func CreatePost(c *gin.Context) {
+	var post models.Post
+	if err := c.ShouldBindJSON(&post); err != nil {
+		c.JSON(400, gin.H{"err": err.Error()})
+		return
+	}
+	if result := DB.Create(&post); result.Error != nil {
+		c.JSON(500, gin.H{"err": result.Error.Error()})
+		return
+	}
+	c.JSON(201, gin.H{"msg": "Post Created Successfullly"})
+
+}
+func GetPostByID(c *gin.Context) {
+	ID := c.Param("id")
+	var post models.Post
+	if result := DB.First(&post, ID); result.Error != nil {
+		c.JSON(500, gin.H{"err": result.Error.Error()})
+		return
+	}
+	c.JSON(200, post)
+}
+
+func DeletePostByID(c *gin.Context) {
+	ID := c.Param("id")
+	var post models.Post
+	if result := DB.Delete(&post, ID); result.Error != nil {
+		c.JSON(500, gin.H{"err": result.Error.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"msg": "Post deleted successfully"})
+}
+func UpdatePostByID(c *gin.Context) {
+	ID := c.Param("id")
+	var post models.Post
+	if result := DB.First(&post, ID); result.Error != nil {
+		c.JSON(404, gin.H{"err": result.Error.Error()})
+		return
+	}
+	if err := c.ShouldBindBodyWithJSON(&post); err != nil {
+		c.JSON(500, gin.H{"err": err})
+		return
+	}
+	DB.Save(&post)
+	c.JSON(200, &post)
 
 }
